94626cc591bcffc54df3f5a259ba1062e8fe388d
pabcorn <pabrock12345@gmail.com>
2022-02-14 01:37:52 +0700

    add container

--- index.php
+++ index.php
@@ -9,12 +9,14 @@
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light"> <a class="navbar-brand" href="#">webpage</a>
-	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+<nav class="navbar navbar-expand-lg navbar-light bg-light"> 
+  
+  <a class="navbar-brand" href="#">NODOG TRAVEL</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
 	  <div class="collapse navbar-collapse" id="navbarSupportedContent1">
 	    <ul class="navbar-nav mr-auto">
 	      <li class="nav-item active"> <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a> </li>
-	      <li class="nav-item"> <a class="nav-link" href="#">Link</a> </li>
+	      <li class="nav-item"> <a class="nav-link" href="#">Gallery</a> </li>
 	      <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Dropdown </a>
 	        <div class="dropdown-menu" aria-labelledby="navbarDropdown1"> <a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a>
 	          <div class="dropdown-divider"></div>
@@ -29,7 +31,14 @@
   </div>
 </nav>
 <div class="container"><img src="รูปภาพ/logo/banner.jpg" width="100%" alt=""/></div>
-
+<div class="container mt-5">
+  <div class="card col-md-4"> <img class="card-img-top" src="images/card-img.png" alt="Card image cap">
+    <div class="card-body col-md-4 shadow">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">รายละเอียด</a> </div>
+    </div>
+</div>
 
 <script src="js/jquery-3.3.1.min.js"></script>
 <script src="js/popper.min.js"></script>
